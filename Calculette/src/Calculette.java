f60e70a5a43556c97f263048b4f95176852c0217
Laurent Laffont <laurent.laffont@gmail.com>
2012-11-05 12:23:57 +0100

    Refactoring: élimination des ifs

--- Calculette/src/Calculette.java
+++ Calculette/src/Calculette.java
@@ -2,34 +2,10 @@
 public class Calculette {
 	private Double operande1 = 0.0;
 	private Double operande2 = 0.0;
-	private Double resultat = 0.0;
-	private String operateur;
+	private Operation operation = new NullOperation();
 
 	public Double getResultat() {
-		this.effectueOperation();
-		return this.resultat;
-	}
-
-	public void effectueOperation() {
-		if ("+".equals(this.operateur)) {
-			this.resultat = this.effectueAddition();	
-			return;
-		}
-		
-		if ("-".equals(this.operateur)) {
-			this.resultat = this.effectueSoustraction();
-			return;
-		} 
-		
-		this.resultat = this.operande2;
-	}
-	
-	public Double effectueSoustraction() {
-		return this.operande1 - this.operande2;	
-	}
-	
-	public Double effectueAddition() {
-		return this.operande1 + this.operande2;	
+		return operation.calcule(this.operande1, this.operande2);
 	}
 
 	public Calculette click(int i) {
@@ -38,11 +14,9 @@ public class Calculette {
 	}
 
 	public Calculette click(String operateur) {
-		this.effectueOperation();
-		this.operateur = operateur;
-		this.operande1 = this.resultat;
+		this.operande1 = this.getResultat();
 		this.operande2 = 0.0;
+		this.operation = Operation.pourOperateur(operateur);
 		return this;
 	}
-
 }
